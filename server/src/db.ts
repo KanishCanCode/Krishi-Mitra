955c873980a72acd421eec3ebc9ba6da4893e2a6
Jagrit Choudhary <jagritchoudhary@Jagrits-MacBook-Air.local>
2025-11-23 03:09:29 +0530

    Add missing db.ts file

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-<<<<<<< HEAD
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
@@ -12,9 +11,3 @@ const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;   // 👈 THIS FIXES YOUR IMPORT ERROR
-=======
-
-const prisma = new PrismaClient();
-
-export default prisma;
->>>>>>> e8c39978d34f0dab72e72cd07075c58d0c37de54
